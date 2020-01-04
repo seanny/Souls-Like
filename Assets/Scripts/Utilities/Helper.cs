4afe9b065abf87206c3bd17042ee16f68ccdce5a
Sean McElholm <seanmcelholm@gmail.com>
2020-01-04 20:51:04 +0000

    Added basic animations

---
+++ Assets/Scripts/Utilities/Helper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helper : MonoBehaviour
+{
+    [Range(0, 1)]
+    public float vertical;
+
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Vertical", vertical);
+    }
+}
