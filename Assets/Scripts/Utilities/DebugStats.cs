31e94bb4838187e2e0268e483ad1a24c196bd31b
Sean McElholm <seanmcelholm@gmail.com>
2020-01-06 22:44:54 +0000

    Added animator root motion setting

---
+++ Assets/Scripts/Utilities/DebugStats.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class DebugStats : MonoBehaviour
+{
+    public TextMeshProUGUI debugStats;
+
+    public float frequency = 1f;
+    public int FramesPerSec { get; protected set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FramesPerSecond());
+    }
+
+    private IEnumerator FramesPerSecond()
+    {
+        while(true)
+        {
+            int lastFrameCount = Time.frameCount;
+            float lastTime = Time.realtimeSinceStartup;
+            yield return new WaitForSeconds(frequency);
+            float timeSpan = Time.realtimeSinceStartup - lastTime;
+            int frameCount = Time.frameCount - lastFrameCount;
+
+            // Display it
+            FramesPerSec = Mathf.RoundToInt(frameCount / timeSpan);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string targetFPS = "N/A";
+        if(Application.targetFrameRate != -1)
+        {
+            targetFPS = Application.targetFrameRate.ToString();
+        }
+        debugStats.text = $"FPS: {FramesPerSec} - Target: {targetFPS}";
+    }
+}
--- Assets/Scripts/Utilities/Helper.cs
+++ Assets/Scripts/Utilities/Helper.cs
@@ -7,17 +7,53 @@ public class Helper : MonoBehaviour
     [Range(0, 1)]
     public float vertical;
 
-    public Animator animator;
+    public bool playAnim;
+    public string[] oneHandedAttacks;
+    public string[] twoHandedAttacks;
+
+    public bool twoHanded;
+    public bool enableRootMotion;
+
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        enableRootMotion = !animator.GetBool("CanMove");
+        animator.applyRootMotion = enableRootMotion;
+        if (enableRootMotion)
+            return;
+
+        animator.SetBool("TwoHanded", twoHanded);
+        if(playAnim)
+        {
+            string targetAnim;
+            int rand;
+
+            if (!twoHanded)
+            {
+                rand = Random.Range(0, oneHandedAttacks.Length);
+                targetAnim = oneHandedAttacks[rand];
+            }
+            else
+            {
+                rand = Random.Range(0, twoHandedAttacks.Length);
+                targetAnim = twoHandedAttacks[rand];
+            }
+
+            vertical = 0;
+            animator.CrossFade(targetAnim, 0.2f);
+            animator.SetBool("CanMove", false);
+            enableRootMotion = true;
+            playAnim = false;
+        }
+
         animator.SetFloat("Vertical", vertical);
     }
 }
