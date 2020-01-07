b2b376fca62b412abf61c8a533c2bd9677fa486d
Sean McElholm <seanmcelholm@gmail.com>
2020-01-07 03:18:06 +0000

    Attempt to fix archer anim (to no avail)

--- Assets/KeepBool.cs
+++ Assets/KeepBool.cs
@@ -6,6 +6,7 @@ public class KeepBool : StateMachineBehaviour
 {
     public string boolName;
     public bool status;
+    public bool resetOnExit = true;
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -15,6 +16,9 @@ public class KeepBool : StateMachineBehaviour
 
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.SetBool(boolName, !status);
+        if(resetOnExit)
+        {
+            animator.SetBool(boolName, !status);
+        }
     }
 }
--- Assets/Scripts/Utilities/Helper.cs
+++ Assets/Scripts/Utilities/Helper.cs
@@ -4,14 +4,33 @@ using UnityEngine;
 
 public class Helper : MonoBehaviour
 {
+    public enum WeaponType
+    {
+        None = 0,
+        OneHanded,
+        TwoHanded,
+        Bow,
+    };
+
+    public enum BowStage
+    {
+        Idle = 0,
+        Aiming,
+        Fire
+    };
+
     [Range(0, 1)]
     public float vertical;
 
     public bool playAnim;
     public string[] oneHandedAttacks;
     public string[] twoHandedAttacks;
+    public string bowIdle;
+    public string bowAimIdle;
+    public string bowAimFire;
 
-    public bool twoHanded;
+    public WeaponType weaponType;
+    public BowStage bowStage;
     public bool enableRootMotion;
 
     Animator animator;
@@ -30,27 +49,45 @@ public class Helper : MonoBehaviour
         if (enableRootMotion)
             return;
 
-        animator.SetBool("TwoHanded", twoHanded);
+        animator.SetInteger("WeaponType", (int)weaponType);
         if(playAnim)
         {
-            string targetAnim;
+            string targetAnim = string.Empty;
             int rand;
 
-            if (!twoHanded)
+            if (weaponType == WeaponType.OneHanded)
             {
                 rand = Random.Range(0, oneHandedAttacks.Length);
                 targetAnim = oneHandedAttacks[rand];
             }
-            else
+            else if (weaponType == WeaponType.TwoHanded)
             {
                 rand = Random.Range(0, twoHandedAttacks.Length);
                 targetAnim = twoHandedAttacks[rand];
             }
+            else if (weaponType == WeaponType.Bow)
+            {
+                if(bowStage == BowStage.Aiming)
+                {
+                    animator.SetBool("AimingBow", true);
+                    targetAnim = bowAimIdle;
+                }
+                else if (bowStage == BowStage.Fire)
+                {
+                    animator.SetBool("AimingBow", false);
+                    targetAnim = bowAimIdle;
+                    bowStage = BowStage.Idle;
+                }
+                else targetAnim = bowIdle;
+            }
+            else
+            {
+                playAnim = false;
+                return;
+            }
 
             vertical = 0;
             animator.CrossFade(targetAnim, 0.2f);
-            animator.SetBool("CanMove", false);
-            enableRootMotion = true;
             playAnim = false;
         }
 
