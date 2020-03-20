01e21da9064e5fdc79e37cf1402a9291525be817
Sean McElholm <seanmcelholm@gmail.com>
2020-03-20 17:33:05 +0000

    Update Ai Condition

---
+++ Assets/Scripts/Utilities/ComparisonCheck.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoulsLike
+{
+    public static class ComparisonCheck
+    {
+        public static bool IsTrue<T, U>(T value1, ComparisonOperator comparisonOperator, U value2)
+            where T : U
+            where U : IComparable
+        {
+            bool retValue = false;
+            switch (comparisonOperator)
+            {
+                case ComparisonOperator.EqualTo:
+                    retValue = value1.CompareTo(value2) == 0;
+                    break;
+                case ComparisonOperator.LessOrEqualTo:
+                    retValue = value1.CompareTo(value2) <= 0;
+                    break;
+                case ComparisonOperator.LessThan:
+                    retValue = value1.CompareTo(value2) < 0;
+                    break;
+                case ComparisonOperator.MoreOrEqualTo:
+                    retValue = value1.CompareTo(value2) >= 0;
+                    break;
+                case ComparisonOperator.MoreThan:
+                    retValue = value1.CompareTo(value2) > 0;
+                    break;
+                case ComparisonOperator.NotEqualTo:
+                    retValue = value1.CompareTo(value2) != 0;
+                    break;
+            }
+            return retValue;
+        }
+    }
+}
