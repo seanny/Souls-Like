41d39cd25750112ecd958f2be4a2d5c690badbe5
Sean McElholm <seanmcelholm@gmail.com>
2020-03-24 01:43:32 +0000

    Actor combat changes.

---
+++ Assets/Scripts/Actors/ActorCombat.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace SoulsLike
+{
+
+    public class ActorCombat : MonoBehaviour
+    {
+        private Animator m_Animator;
+
+        private void Start()
+        {
+            m_Animator = GetComponentInChildren<Animator>();
+        }
+
+        public void SwordAttack()
+        {
+            m_Animator.SetBool("Attacking", true);
+        }
+    }
+}
